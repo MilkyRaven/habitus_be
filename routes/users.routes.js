63730153fb2e713fadba276689074f5de6a51297
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-11-28 16:08:17 +0100

    routes for user following - working progress

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -22,4 +22,33 @@ router.get("/:userId", async (req, res, next) => {
     }
 });
 
+
+// /set following - active user adds visited user-id to Following-Array
+router.put("/:userId/set-following", isAuthenticated, async (req, res, next) => {
+    const user = req.payload._id;
+    const userToFollow =req.params.userId 
+
+    const findUser = await User.findById(user)
+    const following = findUser.following 
+    if (!following.includes(userToFollow) ){
+        const editUser = await User.findByIdAndUpdate(user, { $push: { following: userToFollow} })
+    }   else {
+        const editUser = await User.findByIdAndUpdate(user, { $pull: { following: userToFollow} })
+    } 
+    
+     res.json(findUser)
+})
+
+// /set follower - visited user gets active user added to Follower-Array
+router.put("/:userId/set-follwer", isAuthenticated, async (req, res, next) => {
+    const user = req.payload._id;
+    const { email, password, profileImg, goals, myPreferences } = req.body;
+
+    const editUser = await User.findByIdAndUpdate(user, {email: email, password: password, profileImg: profileImg, goals: goals, myPreferences: myPreferences})
+    console.log(editUser)
+
+    res.json(findUser)
+}) 
+
+
 module.exports = router;
